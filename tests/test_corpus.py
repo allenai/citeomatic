72c260495bb504499dc5787e3363caba68bff7e4
Chandra Bhagavatula <chandrab@allenai.org>
2017-10-18 15:07:52 -0700

    Simple tests to ensure train, validation and test ids in the corpus are isolated (#6)

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import json
+import logging
 import os
 import random
 import time
@@ -135,12 +136,12 @@ def build_test_corpus(source_file, target_file):
                 FieldNames.TITLE: ' '.join(random.sample(WORDS, 10)),
                 FieldNames.ABSTRACT: ' '.join(random.sample(WORDS, 1000)),
                 FieldNames.AUTHORS: [],
-                FieldNames.OUT_CITATIONS:[
-                    str(x) for x in random.sample(range(1000), 10)
+                FieldNames.OUT_CITATIONS: [
+                    str(x) for x in random.sample(range(100), 10)
                 ],
                 FieldNames.YEAR: 2011,
                 FieldNames.PAPER_ID: str(i),
-                FieldNames.VENUE:''
+                FieldNames.VENUE: ''
             }, tf
             )
             tf.write('\n')
@@ -172,6 +173,48 @@ def test_data_gen():
         next(gen)
 
 
+def test_data_isolation():
+    build_test_corpus('/tmp/foo.json', '/tmp/foo.sqlite')
+    corpus = Corpus.load('/tmp/foo.sqlite')
+
+    assert len(set(corpus.train_ids).intersection(set(corpus.valid_ids))) == 0
+    assert len(set(corpus.train_ids).intersection(set(corpus.test_ids))) == 0
+    assert len(set(corpus.valid_ids).intersection(set(corpus.test_ids))) == 0
+
+    featurizer = features.Featurizer(
+        allow_duplicates=False
+    )
+    featurizer.fit(corpus, max_df_frac=1.0)
+    dg = features.DataGenerator(corpus, featurizer)
+
+    query, examples, labels = next(dg._listwise_examples(corpus.train_ids))
+    examples_ids = [doc.id for doc in examples]
+
+    assert len(set(examples_ids).intersection(set(corpus.valid_ids))) == 0
+    assert len(set(examples_ids).intersection(set(corpus.test_ids))) == 0
+
+    dg = features.DataGenerator(corpus, featurizer)
+    query, examples, labels = next(dg._listwise_examples(paper_ids=corpus.valid_ids,
+                                                         candidate_ids=corpus.valid_ids + corpus.train_ids))
+    examples_ids = [doc.id for doc in examples]
+
+    assert len(set(examples_ids).intersection(set(corpus.train_ids))) > 0
+    assert len(set(examples_ids).intersection(set(corpus.test_ids))) == 0
+
+    dg = features.DataGenerator(corpus, featurizer)
+    query, examples, labels = next(dg._listwise_examples(paper_ids=corpus.test_ids,
+                                                         candidate_ids=corpus.valid_ids + corpus.train_ids))
+    examples_ids = [doc.id for doc in examples]
+    assert len(set(examples_ids).intersection(set(corpus.test_ids))) == 0
+
+    dg = features.DataGenerator(corpus, featurizer)
+    query, examples, labels = next(
+        dg._listwise_examples(paper_ids=corpus.test_ids,
+                              candidate_ids=corpus.valid_ids + corpus.train_ids + corpus.test_ids))
+    examples_ids = [doc.id for doc in examples]
+    assert len(set(examples_ids).intersection(set(corpus.test_ids))) != 0
+
 if __name__ == '__main__':
     import pytest
+
     pytest.main([__file__, '-s'])
