fa0aecd43f390aa71f0c8736411440e909e9f6dd
chandrab <chandrab@allenai.org>
2018-02-13 11:57:51 -0800

    Add symlink

--- get-data.sh
+++ get-data.sh
@@ -2,3 +2,10 @@
 LOC=$1
 
 aws s3 sync s3://ai2-s2-research-public/citeomatic-2018-02-12/ ${LOC}
+
+# The `data` directory is hard coded as the base directory in common.py
+# If a different location on disk is provided, we create a symlink from ./data/ -> $LOC
+
+if [[ ${LOC} != 'data' ]]; then
+  ln -s $LOC 'data'
+fi
