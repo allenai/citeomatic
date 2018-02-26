0efde1996d13edee45a88b36f3d1d100ea46e3ec
Chandra Bhagavatula <chandrab@allenai.org>
2018-02-26 09:27:19 -0800

    Update download script (#85)
    * Change data downloading script to use public http request for s3 bucket
    
    * Quick dload script if anything fails

--- get-data.sh
+++ get-data.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+set -x
+set -e
+
 LOC=$1
 
-aws s3 sync s3://ai2-s2-research-public/citeomatic-2018-02-12/ ${LOC}
+wget https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/citeomatic-2018-02-12.tar.gz -P ${LOC}
+tar -xzvf ${LOC}/citeomatic-2018-02-12.tar.gz -C ${LOC}
 
 # The `data` directory is hard coded as the base directory in common.py
 # If a different location on disk is provided, we create a symlink from ./data/ -> $LOC
 
 if [[ ${LOC} != 'data' ]]; then
-  ln -s $LOC 'data'
+  ln -s $LOC/citeomatic-2018-02-12/ 'data'
 fi
